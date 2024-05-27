2468738c15c27128bb075db09faffb588ccad257
Cooper Werner <cooperwerner824@gmail.com>
2024-05-27 12:20:40 -0400

    trying a variety of hashing algorithms

--- hash_testing.cpp
+++ hash_testing.cpp
@@ -1,5 +1,11 @@
 
 #include <iostream>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <byteswap.h>
+
+typedef uint32_t uint32;
 
 char data[][100] = {
 		"Abha",
@@ -417,15 +423,59 @@ char data[][100] = {
 		"Zürich",
 };
 
+
+#include <stdint.h> /* Replace with <stdint.h> if appropriate */
+#define get16bits(d) ((((uint32_t)(((const uint8_t *)(d))[1])) << 8)\
+                       +(uint32_t)(((const uint8_t *)(d))[0]) )
+
+uint32_t SuperFastHash (const char * data, int len) {
+uint32_t hash = len, tmp;
+int rem;
+
+    if (len <= 0 || data == NULL) return 0;
+
+    rem = len & 3;
+    len >>= 2;
+
+    /* Main loop */
+    for (;len > 0; len--) {
+        hash  += get16bits (data);
+        tmp    = (get16bits (data+2) << 11) ^ hash;
+        hash   = (hash << 16) ^ tmp;
+        data  += 2*sizeof (uint16_t);
+        hash  += hash >> 11;
+    }
+
+    /* Handle end cases */
+    switch (rem) {
+        case 3: hash += get16bits (data);
+                hash ^= hash << 16;
+                hash ^= ((signed char)data[sizeof (uint16_t)]) << 18;
+                hash += hash >> 11;
+                break;
+        case 2: hash += get16bits (data);
+                hash ^= hash << 11;
+                hash += hash >> 17;
+                break;
+        case 1: hash += (signed char)*data;
+                hash ^= hash << 10;
+                hash += hash >> 1;
+    }
+
+    /* Force "avalanching" of final 127 bits */
+    hash ^= hash << 3;
+    hash += hash >> 5;
+    hash ^= hash << 4;
+    hash += hash >> 17;
+    hash ^= hash << 25;
+    hash += hash >> 6;
+
+    return hash;
+}
+
 int hash(char *str)
 {
-	// Source: https://stackoverflow.com/a/7666668/5407270
-    uint32_t result = 0x55555555;
-    while (*str) { 
-        result ^= *str++;
-        result = result<<5 | result>>(32-5);
-    }
-    return result;
+	return SuperFastHash(str, strlen(str));
 }
 
 int main()
