87593451c80dc2afcaa5e90fc0d38f3850ccc95b
Cooper Werner <cooperwerner824@gmail.com>
2024-05-27 12:32:09 -0400

    I think this is the fastest single core implementation I can get. Time on my desktop: 2min 35.68s

--- main.c
+++ main.c
@@ -418,7 +418,7 @@ char locations[][100] = {
 #include <stdlib.h>
 #include <math.h>
 #include <string.h>
-#define MAGIC_HASHMAP_SIZE 3281
+#define MAGIC_HASHMAP_SIZE 2636
 #define NUM_CITIES 413
 
 typedef struct
@@ -434,72 +434,13 @@ typedef measurements *magic_hashmap;
 void process_line(char *line, magic_hashmap measures);
 void output_file(magic_hashmap measures);
 
-#include <stdint.h> /* Replace with <stdint.h> if appropriate */
-#define get16bits(d) ((((uint32_t)(((const uint8_t *)(d))[1])) << 8) + (uint32_t)(((const uint8_t *)(d))[0]))
-
-uint32_t SuperFastHash(const char *data, int len)
-{
-	uint32_t hash = len, tmp;
-	int rem;
-
-	if (len <= 0 || data == NULL)
-		return 0;
-
-	rem = len & 3;
-	len >>= 2;
-
-	/* Main loop */
-	for (; len > 0; len--)
-	{
-		hash += get16bits(data);
-		tmp = (get16bits(data + 2) << 11) ^ hash;
-		hash = (hash << 16) ^ tmp;
-		data += 2 * sizeof(uint16_t);
-		hash += hash >> 11;
-	}
-
-	/* Handle end cases */
-	switch (rem)
-	{
-	case 3:
-		hash += get16bits(data);
-		hash ^= hash << 16;
-		hash ^= ((signed char)data[sizeof(uint16_t)]) << 18;
-		hash += hash >> 11;
-		break;
-	case 2:
-		hash += get16bits(data);
-		hash ^= hash << 11;
-		hash += hash >> 17;
-		break;
-	case 1:
-		hash += (signed char)*data;
-		hash ^= hash << 10;
-		hash += hash >> 1;
-	}
-
-	/* Force "avalanching" of final 127 bits */
-	hash ^= hash << 3;
-	hash += hash >> 5;
-	hash ^= hash << 4;
-	hash += hash >> 17;
-	hash ^= hash << 25;
-	hash += hash >> 6;
-
-	return hash;
-}
-
 size_t hash(const char *key)
 {
-#if 0
- 	return SuperFastHash(key, strlen(key)) % MAGIC_HASHMAP_SIZE;
-#else
 	unsigned long hash = 5381;
 	char c;
 	while ((c = *key++))
 		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 	return hash % MAGIC_HASHMAP_SIZE;
-#endif
 }
 
 int main()
